fa2b0c48da845fa99a8ffe8527642b5b67a11015
sina <s1n4.ne1331@gmail.com>
2023-11-25 20:54:15 +0330

    ADD: add runner compose & script

---
+++ gitlab-runner/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+registration_token= <<PLACE HOLDER>>
+url= <<PLACE HOLDER>>
+
+docker exec -it gitlab-runner \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url ${url} \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode web_net
